7341235077486fa0138d8d94e871db0b4a30ea91
yokoko <yoko.syun.1408@gmail.com>
2021-01-14 12:29:38 +0900

    fixed train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python train.py --maxdisp 192 \
                --model concatNet \
                --datapath /media/yoko/SSD-PGU3/workspace/datasets/KITTI/data_scene_flow/training/ \
-               --epochs 100 \
+               --epochs 300 \
                --savemodel ./result \
                --seed 1
                
